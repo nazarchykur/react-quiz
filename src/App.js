ce3a2f0e4d9519bd1e246272551e3ba52acbdf28
nazarchykur <nazarchykur@gmail.com>
2023-09-21 13:09:10 +0300

    added an example of Loading Questions from a Fake API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+
+  // "loading", "error", "ready", "active", "finished"
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailde":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function() {
+    fetch("http://localhost:5000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailde" }));
+  }, []);
+
   return (
     <div className="App">
       <Header />
