3fa6e875ba5b5a78a5a464c1b7cf52f61009aa41
nazarchykur <nazarchykur@gmail.com>
2023-09-21 17:46:38 +0300

    added an example of Finishing a Quiz

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,25 @@
+function FinishScreen({ points, maxPossiblePoints, highscore }) {
+  const percentage = Math.ceil((points / maxPossiblePoints) * 100);
+
+  let emoji;
+  if (percentage === 100) emoji = "🤴 🏆 ";
+  if (percentage >= 80 && percentage < 100) emoji = "😎 🥈";
+  if (percentage > 60 && percentage < 80) emoji = "🤨 🥉";
+  if (percentage > 40 && percentage < 60) emoji = "😉";
+  if (percentage >= 0 && percentage < 40) emoji = "🐒";
+  if (percentage === 0) emoji = "🤧";
+
+  return (
+    <>
+      <p className="result">
+        <span>🎉 Congratulation! </span>
+        <br />
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
+        <strong>{maxPossiblePoints}</strong> ({percentage}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+    </>
+  );
+}
+
+export default FinishScreen;
