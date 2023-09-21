5423cc48ef7fc6ae6cac110858e7676eaf1063c6
nazarchykur <nazarchykur@gmail.com>
2023-09-21 16:07:06 +0300

    added an example of Handling New Answers

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,23 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswer = answer !== null;
+
   return (
     <div className="options">
       {question.options.map((option, index) => (
-        <button key={index} className="btn btn-option">
+        <button
+          key={index}
+          className={`btn btn-option 
+          ${index === answer ? "answer" : ""} 
+          ${
+            hasAnswer
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          disabled={hasAnswer}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
